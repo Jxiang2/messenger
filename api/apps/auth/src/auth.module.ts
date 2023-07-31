da2ccc8b5067bcb9f067dba7895fd139cfa297c0
Jxiang2 <xjyhenry@gmail.com>
2023-07-30 23:54:29 -0400

    prisma

--- api/apps/auth/src/auth.module.ts
+++ api/apps/auth/src/auth.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
+import { PrismaModule } from "@app/shared";
 
 @Module({
   imports: [
+    PrismaModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ".env",
--- api/apps/auth/src/auth.service.ts
+++ api/apps/auth/src/auth.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "@app/shared/prisma/prisma.service";
 
 @Injectable()
 export class AuthService {
+  constructor(readonly prismaService: PrismaService) {}
+
   async getUsers() {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    return "Getting...";
+    return await this.prismaService.user.findMany();
   }
 
   async postUser() {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    return "Posting...";
+    return await this.prismaService.user.create({
+      data: {
+        name: "xjy",
+        email: "123@gmail.com",
+      },
+    });
   }
 }
