50f1788d2d7ecc61aeb99a3657112c92ee5ac636
Jxiang2 <xjyhenry@gmail.com>
2023-08-05 16:00:20 -0400

    chore

--- api/apps/gateway/src/auth/auth.controller.ts
+++ api/apps/gateway/src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Inject, Post } from "@nestjs/common";
 import { ClientProxy, RmqRecordBuilder } from "@nestjs/microservices";
 import { RMQ_SERVICE } from "@app/shared/rmq/constant";
-import { UserDto } from "@app/shared/dto";
+import { RegisterUserDto } from "@app/shared/dto/user.dto";
 
 @Controller("auth")
 export class AuthController {
@@ -16,8 +16,10 @@ export class AuthController {
   }
 
   @Post("/register")
-  register(@Body() newUser: UserDto) {
-    const payload = new RmqRecordBuilder<UserDto>().setData(newUser).build();
+  register(@Body() newUser: RegisterUserDto) {
+    const payload = new RmqRecordBuilder<RegisterUserDto>()
+      .setData(newUser)
+      .build();
     return this.authService.send({ cmd: "register" }, payload);
   }
 }
